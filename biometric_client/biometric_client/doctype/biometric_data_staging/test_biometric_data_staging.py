f5bb6c83f338372ecb6742828b3ed2194bee8ab1
aakvatech <35020381+aakvatech@users.noreply.github.com>
2026-09-25 15:15:01 +0300

    test: cover biometric punch type normalization
    (cherry picked from commit 01a08259e9b580450e6db8c3eebbcb4881dc5802)

--- biometric_client/biometric_client/doctype/biometric_data_staging/test_biometric_data_staging.py
+++ biometric_client/biometric_client/doctype/biometric_data_staging/test_biometric_data_staging.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, Asha Melius Kisonga and Contributors
 # See license.txt
 
-# import frappe
 from frappe.tests.utils import FrappeTestCase
+
+from biometric_client.biometric_client.doctype.biometric_data_staging.biometric_data_staging import (
+    normalize_log_type,
+)
 
 
 class TestBiometricDataStaging(FrappeTestCase):
-	pass
+    def test_normalize_log_type_preserves_supported_values(self):
+        self.assertEqual(normalize_log_type("IN"), "IN")
+        self.assertEqual(normalize_log_type("OUT"), "OUT")
+
+    def test_normalize_log_type_maps_auto_and_blank_to_blank(self):
+        self.assertEqual(normalize_log_type("AUTO"), "")
+        self.assertEqual(normalize_log_type(""), "")
